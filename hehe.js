28028cfc0e5a70ebcacce37fc46a62ec10da9ea8
mac <mac@macs-MacBook-Pro.local>
2025-12-02 22:21:55 +0700

    hehe

---
+++ hehe.js
@@ -0,0 +1,6 @@
+let a = [1, "Nguyễn Văn Minh", "74DCTT22583", "pro"
+        ,2, "Nguyễn Hoàng Hiệp", "74DCTT22583", "pro"
+        ,3, "Nguyễn Anh Tuấn", "74DCTT22583", "pro"
+        ,4, "Hồ Minh Nhât", "74DCTT22583", "pro"
+];
+    console.log(a);
